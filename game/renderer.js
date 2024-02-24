a17c38743c50e8f3f9d9b72a6a71baeb231b624b
AaryanK101 <94056317+AaryanK101@users.noreply.github.com>
2024-02-24 16:17:30 +0000

    changes

--- game/renderer.js
+++ game/renderer.js
@@ -1,7 +1,23 @@
 class Renderer {
 
     render(gameState) {
-        // render board 
-    }
+        // Defining the canvas dimensions for fixed phone size
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
+    
+        // Porpoerties of the borders of game
+        const borderWidth = 20;
+        const borderColor = "Black";
+
+        context.fillStyle = borderColor;
+        // Top Border
+        context.fillRect(0, 0, canvas.width, borderWidth);
+        //Bottom Border
+        context.fillRect(0, canvas.height - borderWidth, canvas.width, borderWidth);
+        // Left Border
+        context.fillRect(0, 0, borderWidth, canvas.height);
+        //Right Border
+        context.fillRect(canvas.width - borderWidth, 0, borderWidth, canvas.height);
+    }
 }
